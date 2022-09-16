e8933c98d14fc8326365d0df287bd26cfc26151b
HiraZaidii <hirazaidi@hotmail.com>
2022-09-16 10:34:15 +0000

    define global vars

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+
+
+
 """
 BATTLESHIP
-grid of 10x10 with 8 ships in different lengths
+grid of 10x10 with 6 ships in different lengths
 
 LEGEND
 . = water or empty space
@@ -8,4 +11,20 @@ LEGEND
 x = part hit with a bullet
 # = missed bullet
 """
+# global var for grid
+grid =[[]]
+# var for grid size
+grid_size = 10
+# var for number of ships
+num_of_ships = 6
+# var for bullets left
+bullets_left = 40
+# var for game over
+game_over = False
+# var for num of ships sunk
+num_of_ships_sunk = 0
+# var for position of ships
+ship_positions = [[]]
+# var for alphabet
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
