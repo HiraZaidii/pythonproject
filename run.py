d234262d3567bd53b44422baefbfebb07c1bf5fa
HiraZaidii <hirazaidi@hotmail.com>
2022-09-19 09:30:14 +0000

    add base for grid positions of ship

--- run.py
+++ run.py
@@ -40,4 +40,38 @@ def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
             grid[r][c] != ".":
             all_valid= False
             break
-        
+        if all_valid: 
+            ship_positions.append([start_row, end_row, start_col, end_col])
+            for r in range(start_row, end_row):
+                for c in range(start_col, end_col):
+                    grid[r][c] = "0"
+                    return all_valid
+
+"""place ship on grid"""
+def try_to_place_ship_on_grid(row, col, direction, length):
+    global grid_size
+
+    start_row, end_row, start_col, end_col = row, row + 1, col, col + 1
+    if direction == "left":
+        if col - length < 0:
+            return False
+            start_col = col - length + 1
+
+    elif direction == "right":
+            if col + length >= grid_size:
+                return False
+                end_col = col + length
+
+     elif direction == "up":
+        if row - length < 0:
+                        return False
+                        start_row = row - length + 1
+
+     elif direction = "left":
+        if row + length >= grid_size:
+                                return False
+                                end_row  = row + length
+
+return validate_grid_and_place_ship(start_row, end_row, start_col, end_col)
+
+
