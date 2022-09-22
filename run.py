93ef706e8bcec24e9a494ed492475f1ab3fcd506
HiraZaidii <hirazaidi@hotmail.com>
2022-09-22 23:17:08 +0000

    deleted old undeployable code and debug new battleship code

--- run.py
+++ run.py
@@ -7,11 +7,14 @@ for x in range(5):
     board.append(["O"] * 5)
 """ create board of 5 by 5 """
 
+
 def print_board(board):
     for row in board:
         print" ".join(row)
 
+
 print_board(board)
+
 
 def random_row(board):
     return randint(0, len(board) - 1)
@@ -22,10 +25,26 @@ def random_col(board):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
+
 for turn in range(4):
     print "Turn", turn + 1
     guess_row = int(raw_input("Which row?: "))
     guess_col = int(raw_input("Which col?: "))
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Wow, you hit the right spot!"
+        break
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Haha, wrong spot!"
+        elif(board[guess_row][guess_col] == "X"):
+            print "You've already hit this one!"
+        else:
+            print "Too bad! You didn't hit the battleship"
+            board[guess_row][guess_col] = "X"
+            print_board(board)
+        if (turn==3):
+            print "Better luck next time! This game is over"
+
 
 
 def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
