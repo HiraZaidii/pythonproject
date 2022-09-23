0aecbedab194a7b0cfa1bf3e1eb0dc79d7fb0f02
HiraZaidii <hirazaidi@hotmail.com>
2022-09-23 00:25:13 +0000

    debug too long lines and indentation

--- run.py
+++ run.py
@@ -15,30 +15,34 @@ def print_board(board):
 
 print_board(board)
 
-
-def random_row(board):
-    return randint(0, len(board) - 1)
 """to acces random rows"""
 
 
-def random_col(board):
-    return randint(0, len(board), - 1)
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+
 """to acces random cols"""
+
+
+def random_col(board):
+    return randint(0, len(board) - 1)
+
+
 ship_row = random_row(board)
 ship_col = random_col(board)
 
 
 for turn in range(4):
     print("Turn"), turn + 1
-    guess_row = int(raw_input("Which row?: "))
-    guess_col = int(raw_input("Which col?: "))
+    guess_row = int(input("Which row?: "))
+    guess_col = int(input("Which col?: "))
     if guess_row == ship_row and guess_col == ship_col:
         print("Wow, you hit the right spot!")
         break
     else:
-        if (guess_row < 0 or guess_row > 4) or
-(guess_col < 0 or guess_col > 4):
-        print("Haha, wrong spot!")
+        if(guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print("Haha, wrong spot!")
         elif(board[guess_row][guess_col] == "X"):
             print("You've already hit this one!")
         else:
