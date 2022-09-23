6ceb1739a0b018d17d0dadac990f7b06b0144db9
HiraZaidii <hirazaidi@hotmail.com>
2022-09-23 00:01:35 +0000

    saved changes for debug

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ for x in range(5):
 
 def print_board(board):
     for row in board:
-        print" ".join(row)
+        print(" ").join(row)
 
 
 print_board(board)
@@ -33,17 +33,17 @@ for turn in range(4):
     guess_row = int(raw_input("Which row?: "))
     guess_col = int(raw_input("Which col?: "))
     if guess_row == ship_row and guess_col == ship_col:
-        print "Wow, you hit the right spot!"
+        print("Wow, you hit the right spot!")
         break
     else:
         if (guess_row < 0 or guess_row > 4) or
 (guess_col < 0 or guess_col > 4):
-        print "Haha, wrong spot!"
+        print("Haha, wrong spot!")
         elif(board[guess_row][guess_col] == "X"):
-            print "You've already hit this one!"
+            print("You've already hit this one!")
         else:
-            print "Too bad! You didn't hit the battleship"
+            print("Too bad! You didn't hit the battleship")
             board[guess_row][guess_col] = "X"
             print_board(board)
         if (turn == 3):
-            print "Better luck next time! This game is over"
+            print("Better luck next time! This game is over")
